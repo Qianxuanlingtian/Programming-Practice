c86412224867aa2f1f4068eac03c3598f06a4d77
Qianxuanlingtian <112190152+Qianxuanlingtian@users.noreply.github.com>
2023-11-30 19:07:46 +0800

    test

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-init = input('请输入需要计算第几项斐波那契数列（需要是一个正整数）：')
-while True:
-    if not init.isalnum():
-        init = input('输入不正确，请重新输入一个正整数：')
+# import pytest
+# init = input('请输入需要计算第几项斐波那契数列（需要是一个正整数）：')
+# while True:
+#     if not init.isalnum():
+#         init = input('输入不正确，请重新输入一个正整数：')
 
 def fib(n):
     num1, num2 = 0, 1
@@ -10,4 +11,5 @@ def fib(n):
     print(num1)
 
 print('结果为：')
-fib(int(init))
+# fib(int(init))
+fib(10)
