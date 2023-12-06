d988dbaeb6a498b239e9f514c9fba67739c3ae9f
Qianxuanlingtian <112190152+Qianxuanlingtian@users.noreply.github.com>
2023-12-06 08:58:11 +0800

    Update test_test.py

--- test_test.py
+++ test_test.py
@@ -1,9 +1,6 @@
-import test
+from test import *
 
 class TestFlow:
   def test_01(self):
-    test.fib(13)
-  def test_02(self):
-    test.fib(12)
-  def test_03(self):
-    test.fib(10)
+    e = E()
+    print(e.x)
